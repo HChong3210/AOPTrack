f46c6552ae53a18c8df3b5848f0c1f90e5204879
HChong <hchong7557@gmail.com>
2017-02-24 10:20:07 +0800

    Initial Commit

---
+++ AOPTrack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AOPTrack
+//
+//  Created by HChong on 2017/2/24.
+//  Copyright © 2017年 HChong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
